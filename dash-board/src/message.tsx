26ac7aad9562af1293d0870da6c14e4dacaba638
supratheeka <supratheekayadav@gmail.com>
2025-07-18 17:13:22 +0530

    Add untracked files

---
+++ dash-board/src/message.tsx
@@ -0,0 +1,21 @@
+
+function Menu() {
+    return (
+        <header className="menu-header">
+        <h1>Welcome to Srika Restaurant</h1>
+        
+        <nav className="menu-bar">
+            <ul>
+                <li>Pizza</li>
+                <li>Burger</li>
+                <li>Pasta</li>
+                <li>Fries</li>
+                <li>Drinks</li>
+            </ul>
+        </nav>
+        </header>
+        
+    );
+}
+
+export default Menu;
